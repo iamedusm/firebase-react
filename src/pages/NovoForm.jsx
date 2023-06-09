10a43c908cc9d8d9ea74c8e156afde56aca01dc4
Eduardo Fernandes <iamedusm@gmail.com>
2023-06-09 20:26:24 -0300

    [UPDATE] criação do TaskService

--- src/pages/NovoForm.jsx
+++ src/pages/NovoForm.jsx
@@ -1,4 +1,21 @@
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+import { insereTarefa } from '../services/TaskService'
+
 export default function NovoForm() {
+const {register, handleSubmit } = useForm()
+const navigate = useNavigate()
+
+async function onSubmit(data) {
+  try {
+    await insereTarefa(data)
+    navigate('/')
+  } catch (error) {
+    console.log(error.message)
+  }
+
+}
+
   return (
     <>
     </>
--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -1,15 +1,19 @@
 import { urlApi } from "./FirebaseConfig"
 
-function listaTarefas() {
+export async function listaTarefas() {
     fetch(urlApi)
     .then((response) => response.json)
     .then((data) => console.log(data))
     .catch((error) => { throw Error(`Erro - ${error}`) })
 }
 
-function insereTarefa(tarefa) {
+export async function insereTarefa(tarefa) {
     fetch(urlApi, {
         method: 'POST',
-        body: JSON.stringify()
+        body: JSON.stringify(tarefa),
+        headers: {
+            'Content-type': 'application/json'
+        }
     })
+    .catch((error) => {throw Error(`Deu Erro: ${error}`)})
 }
